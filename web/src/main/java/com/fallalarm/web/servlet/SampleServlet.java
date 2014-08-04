c2de399a952ce49d9f6c75f2b3be49982952c40d
Gnanaprakash Ramswami <remindprakash@hotmail.com>
2014-08-03 18:24:52 -0700

    initial version

---
+++ web/src/main/java/com/fallalarm/web/servlet/SampleServlet.java
@@ -0,0 +1,7 @@
+package com.fallalarm.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class SampleServlet extends HttpServlet {
+
+}
---
+++ web/src/main/java/com/fallalarm/web/util/CommonUtil.java
@@ -0,0 +1,5 @@
+package com.fallalarm.web.util;
+
+public class CommonUtil {
+
+}
